f715cd54b051218d3efc7d304bb69a301323a816
shivam jha <shivam.jha@mapmyindia.com>
2024-05-10 11:01:09 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,5 +39,24 @@ export class AppComponent {
     // this.modifiedModel.code = editor;
     console.log(JSON.stringify(editor));
   }
-  
+
+  runCode() {
+    const data = fetch('https://api.jdoodle.com/v1/execute', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        
+      },
+      body: JSON.stringify({
+        script: "const data=['1','2','3','4','5'];\ndata.forEach((data)=>{\n    console.log(data)\n})",
+        language: 'nodejs',
+        versionIndex: '0',
+        clientId: '5ba65790e79d996a95c0dabd77ba55c9',
+        clientSecret: '4a2a62d3395ef874b9179a659bfc3f3110ed07c7b1196fe2ab5c5e2a9fef9e98',
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+    }
 } 
